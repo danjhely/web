b2f9cd5dfec4e093c68bf1de9c9fb25e978f340f
danjhely <danghelycanov@gmail.com>
2024-11-10 08:41:22 -0500

    Mi Hoja de vida

---
+++ script.js
@@ -0,0 +1,4 @@
+// Selecciona el botón y añade un evento para mostrar el mensaje
+document.getElementById('btnMensaje').addEventListener('click', function() {
+    alert('¡Bienvenido a mi hoja de vida! toma una galletita y contratame tenkiusss. :)');
+})
